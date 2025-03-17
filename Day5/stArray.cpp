e623341f72b39cbfb39129bda2e5d709859ca8d1
JYK <jykim9226@gmail.com>
2025-03-17 17:07:58 +0900

    5일차

---
+++ Day5/stArray.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+typedef struct addr {
+    char name[20];
+    int age;
+    char tel[20];
+    char addr[100];
+} Addr;
+
+// print_list 함수 정의
+void print_list(Addr list[]) {
+    for (int i = 0; i < 3; i++) {
+        printf("%s, %d, %s, %s\n", list[i].name, list[i].age, list[i].tel, list[i].addr);
+    }
+}
+
+int main() {
+    // 구조체 배열 초기화
+    Addr list[3] = {
+        {"홍길동", 100, "010-1234-4321", "몰라"},
+        {"김철수", 33, "010-2345-2222", "부산"},
+        {"김영희", 23, "010-3456-3333", "서울"}
+    };
+
+    // print_list 함수를 호출하여 배열을 출력
+    print_list(list);
+
+    return 0;
+}
--- Day5/struct6.cpp
+++ Day5/struct6.cpp
@@ -1,33 +1,33 @@
-﻿#include <stdio.h>
-
-// Vision이라는 구조체를 정의, 왼쪽 시력(left)과 오른쪽 시력(right)을 저장
-typedef struct vision {         // typedef struct = class
-    double left;
-    double right;
-
-    void move() {};
-} Vision;
-
-// Vision 구조체를 입력받아 left와 right를 교환한 후 반환하는 함수
-Vision exchange(Vision v) {
-    double temp = v.left;  
-    v.left = v.right;      
-    v.right = temp;        
-    return v;              // 교환된 구조체 반환
-}
-
-int main() {
-    Vision rot;    // Vision 타입의 변수 rot 선언
-
-    
-    printf("시력 입력 (왼쪽 오른쪽) > ");
-    scanf_s("%lf %lf", &rot.left, &rot.right); // 실수형(double) 입력을 받아 rot에 저장
-
-    // exchange 함수를 호출하여 시력을 교환하고, 결과를 다시 rot에 저장
-    rot = exchange(rot);
-
-    // 교환된 시력을 출력
-    printf("바뀐 시력: %.1lf %.1lf\n", rot.left, rot.right);
-
-    return 0;  // 프로그램 종료
-}
+﻿//#include <stdio.h>
+//
+//// Vision이라는 구조체를 정의, 왼쪽 시력(left)과 오른쪽 시력(right)을 저장
+//typedef struct vision {         // typedef struct = class
+//    double left;
+//    double right;
+//
+//    void move() {};
+//} Vision;
+//
+//// Vision 구조체를 입력받아 left와 right를 교환한 후 반환하는 함수
+//Vision exchange(Vision v) {
+//    double temp = v.left;  
+//    v.left = v.right;      
+//    v.right = temp;        
+//    return v;              // 교환된 구조체 반환
+//}
+//
+//int main() {
+//    Vision rot;    // Vision 타입의 변수 rot 선언
+//
+//    
+//    printf("시력 입력 (왼쪽 오른쪽) > ");
+//    scanf_s("%lf %lf", &rot.left, &rot.right); // 실수형(double) 입력을 받아 rot에 저장
+//
+//    // exchange 함수를 호출하여 시력을 교환하고, 결과를 다시 rot에 저장
+//    rot = exchange(rot);
+//
+//    // 교환된 시력을 출력
+//    printf("바뀐 시력: %.1lf %.1lf\n", rot.left, rot.right);
+//
+//    return 0;  // 프로그램 종료
+//}
